c6446fd17bf2e1a9483e5bbdb62798f21904a50b
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-03 23:45:02 +0200

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,12 @@
 
+SELECT 
+    p.property_id,
+    p.name,
+    p.description,
+    p.location
+FROM Property p
+WHERE (
+    SELECT AVG(r.rating)
+    FROM Review r
+    WHERE r.property_id = p.property_id
+) > 4.0;
