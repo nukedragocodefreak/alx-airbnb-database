4092a9437084e1f6a610d09acf8105e3a4ce284e
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-04 14:18:10 +0200

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -15,3 +15,13 @@ CREATE INDEX idx_booking_user_id ON Booking(user_id);
 CREATE INDEX idx_booking_status ON Booking(status);
 CREATE INDEX idx_booking_start_date ON Booking(start_date);
 CREATE INDEX idx_booking_end_date ON Booking(end_date);
+
+
+EXPLAIN ANALYZE
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name;
