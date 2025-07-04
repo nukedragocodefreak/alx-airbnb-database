4d66c66030ec86799a4ef8f8c44673c085a2058a
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-04 14:20:20 +0200

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -31,3 +31,27 @@ FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+
+EXPLAIN
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
