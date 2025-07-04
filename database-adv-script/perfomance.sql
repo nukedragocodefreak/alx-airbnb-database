a04c2bdf964807bd3ddeab3ed1f48af8b21369c8
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-04 14:19:56 +0200

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,33 @@
 
+-- performance.sql
+
+-- Step 1: Retrieve all bookings with related user, property, and payment details
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+
+    -- User Details
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+
+    -- Property Details
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+
+    -- Payment Details
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method
+
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
